296b07c9bbe7388526b14f5210540db7e9173b02
Mateus Morais <mateus.morais@navstar.com>
2023-01-10 19:20:21 -0800

    Adding comments to the handler class.

--- DoneButtonForIOSKeyboard/Platforms/iOS/EntryHandler.cs
+++ DoneButtonForIOSKeyboard/Platforms/iOS/EntryHandler.cs
@@ -1,4 +1,5 @@
-﻿#if IOS
+﻿//this guarantees that this handler will only be used by the iOS application
+#if IOS
 using Microsoft.Maui.Platform;
 using UIKit;
 using System.Drawing;
@@ -11,13 +12,16 @@ public class EntryHandler : Microsoft.Maui.Handlers.EntryHandler
     {
         base.ConnectHandler(platformView);
 
+        //Create a new toolbar.
         var toolbar = new UIToolbar(new RectangleF(0.0f, 0.0f, 50.0f, 44.0f));
 
+        //Create a new UIBarButton with a delegate to clear the focus on the Entry by calling a method that forces the text to stop being edited.
         var doneButton = new UIBarButtonItem(UIBarButtonSystemItem.Done, delegate
         {
             this.PlatformView.EndEditing(true);
         });
 
+        //Add the button to the toolbar previosly created
         toolbar.Items = new UIBarButtonItem[]
         {
                 new UIBarButtonItem (UIBarButtonSystemItem.FlexibleSpace),
